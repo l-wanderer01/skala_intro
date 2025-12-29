22fd73022787eedc0ba0bb7b5c052e8b5d752b4d
장재훈 <wkdwogns@dankook.ac.kr>
2025-12-29 16:34:29 +0900

    echo 프로그램 개선

--- echo.py
+++ echo.py
@@ -1,3 +1,8 @@
-# 사용자 입력을 받아 그대로 출력하는 프로그램
-sentence = input("문장을 입력하세요: ")
-print(sentence)
+while True:
+    sentence = input("문장을 입력하세요 (!quit 입력 시 종료): ")
+
+    if sentence == "!quit":
+        print("프로그램을 종료합니다.")
+        break
+
+    print(sentence)
